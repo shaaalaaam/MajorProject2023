2a8f2aa4aa650f00c99daa3f371b0a7697e1b213
shaaalaaam <shaaalaaam@gmail.com>
2023-04-25 12:12:31 +0530

    Update

---
+++ traffic.js
@@ -0,0 +1,24 @@
+const counts = document.querySelectorAll('.count');
+const images = document.querySelectorAll('.image');
+
+function updateImages() {
+    counts.forEach((count, index) => {
+        const value = parseInt(count.textContent);
+        if (value === 0) {
+            images[index].src = 'images/red.png';
+        } else if (value < 10) {
+            images[index].src = 'images/yellow.png';
+        } else {
+            images[index].src = 'images/green.png';
+        }
+    });
+}
+
+setInterval(() => {
+    counts.forEach((count) => {
+        count.textContent = Math.floor(Math.random() * 20);
+    });
+    updateImages();
+}, 2000);
+
+updateImages();
