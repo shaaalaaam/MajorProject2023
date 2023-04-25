7f3864ba532732610ab7777ba534eb321ce2d6dc
shaaalaaam <shaaalaaam@gmail.com>
2023-04-25 12:39:14 +0530

    Till random count is updating the traffic lights

--- traffic.js
+++ traffic.js
@@ -1,24 +1,27 @@
-const counts = document.querySelectorAll('.count');
-const images = document.querySelectorAll('.image');
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("My JavaScript file is running.");
+    const counts = document.querySelectorAll('.count');
+    const images = document.querySelectorAll('.image');
 
-function updateImages() {
-    counts.forEach((count, index) => {
-        const value = parseInt(count.textContent);
-        if (value === 0) {
-            images[index].src = 'images/red.png';
-        } else if (value < 10) {
-            images[index].src = 'images/yellow.png';
-        } else {
-            images[index].src = 'images/green.png';
-        }
-    });
-}
+    function updateImages() {
+        counts.forEach((count, index) => {
+            const value = parseInt(count.textContent);
+            if (value === 0) {
+                images[index].src = 'images/red.png';
+            } else if (value < 5) {
+                images[index].src = 'images/yellow.png';
+            } else {
+                images[index].src = 'images/green.png';
+            }
+        });
+    }
 
-setInterval(() => {
-    counts.forEach((count) => {
-        count.textContent = Math.floor(Math.random() * 20);
-    });
+    setInterval(() => {
+        counts.forEach((count) => {
+            count.textContent = Math.floor(Math.random() * 20);
+        });
+        updateImages();
+    }, 2000);
+
     updateImages();
-}, 2000);
-
-updateImages();
+});
