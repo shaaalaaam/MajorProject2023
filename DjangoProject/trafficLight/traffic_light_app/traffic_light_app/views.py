e46df0d9bd509b66488e83170ba2d0a7b32ea3b0
shalam <73836150+shaaalaaam@users.noreply.github.com>
2023-04-28 13:38:44 +0530

    Update views.py

--- DjangoProject/trafficLight/traffic_light_app/traffic_light_app/views.py
+++ DjangoProject/trafficLight/traffic_light_app/traffic_light_app/views.py
@@ -4,6 +4,103 @@ import cv2
 import numpy as np
 
 
+def compute_best_threshold(box_list, prev_box_list):
+    """
+    Computes the best overlap threshold between two sets of bounding boxes.
+    
+    Args:
+    - box_list: list of lists of 4 integers, representing the (left, top, width, height) of the current bounding boxes
+    - prev_box_list: list of lists of 4 integers, representing the (left, top, width, height) of the previous bounding boxes
+    
+    Returns:
+    - best_threshold: float between 0 and 1, representing the best overlap threshold
+    """
+    thresholds = [0.1, 0.3, 0.5, 0.7, 0.9]  # List of candidate thresholds
+    best_count = 0
+    best_threshold = 0
+
+    for threshold in thresholds:
+        count = 0
+
+        for i in range(len(box_list)):
+            for j in range(len(prev_box_list)):
+                if box_overlap(box_list[i], prev_box_list[j]) > threshold:
+                    count += 1
+
+        if count > best_count:
+            best_count = count
+            best_threshold = threshold
+
+    return best_threshold
+
+
+def box_overlap(box, prev_box, threshold):
+    """
+    Computes the overlap (intersection over union) between two bounding boxes and returns True if the overlap is greater than the threshold.
+    
+    Args:
+    - box: list of 4 integers, representing the (left, top, width, height) of the current bounding box
+    - prev_box: list of 4 integers, representing the (left, top, width, height) of the previous bounding box
+    - threshold: float between 0 and 1, representing the minimum overlap required to consider the boxes as the same object
+    
+    Returns:
+    - overlap: bool, representing whether the overlap between the two bounding boxes is greater than the threshold or not
+    """
+    # Compute the coordinates of the intersection rectangle
+    x1 = max(box[0], prev_box[0])
+    y1 = max(box[1], prev_box[1])
+    x2 = min(box[0] + box[2], prev_box[0] + prev_box[2])
+    y2 = min(box[1] + box[3], prev_box[1] + prev_box[3])
+
+    # Compute the area of intersection rectangle
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+
+    # Compute the area of both the boxes
+    box_area = box[2] * box[3]
+    prev_box_area = prev_box[2] * prev_box[3]
+
+    # Compute the area of union rectangle
+    union_area = box_area + prev_box_area - intersection_area
+
+    # Compute the overlap (intersection over union)
+    overlap = intersection_area / union_area if union_area > 0 else 0
+
+    # Check if the overlap is greater than the threshold
+    return overlap > threshold
+
+
+# def box_overlap(box, prev_box):
+#     """
+#     Computes the overlap (intersection over union) between two bounding boxes.
+    
+#     Args:
+#     - box: list of 4 integers, representing the (left, top, width, height) of the current bounding box
+#     - prev_box: list of 4 integers, representing the (left, top, width, height) of the previous bounding box
+    
+#     Returns:
+#     - overlap: float between 0 and 1, representing the overlap between the two bounding boxes
+#     """
+#     # Compute the coordinates of the intersection rectangle
+#     x1 = max(box[0], prev_box[0])
+#     y1 = max(box[1], prev_box[1])
+#     x2 = min(box[0] + box[2], prev_box[0] + prev_box[2])
+#     y2 = min(box[1] + box[3], prev_box[1] + prev_box[3])
+
+#     # Compute the area of intersection rectangle
+#     intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+
+#     # Compute the area of both the boxes
+#     box_area = box[2] * box[3]
+#     prev_box_area = prev_box[2] * prev_box[3]
+
+#     # Compute the area of union rectangle
+#     union_area = box_area + prev_box_area - intersection_area
+
+#     # Compute the overlap (intersection over union)
+#     overlap = intersection_area / union_area if union_area > 0 else 0
+
+#     return overlap
+
 def index(request):
     # Call start_vehicle_detection function from within a view
     vehicle_count = start_vehicle_detection()
@@ -11,13 +108,11 @@ def index(request):
     return render(request, "indexx.html", context)
 
     
-
 def start_vehicle_detection():
     # vehicle_count = 0
     # Load the YOLOv3 model and initialize the video capture device
     net = cv2.dnn.readNetFromDarknet(
         '/Users/shalam/Downloads/MajorProject/yolov3.cfg', '/Users/shalam/Downloads/MajorProject/yolov3.weights')
-
 
     # Get the output layer names
     layer_names = net.getLayerNames()
@@ -29,8 +124,10 @@ def start_vehicle_detection():
     # Open video capture device
     cap = cv2.VideoCapture(0)
 
+    detected_vehicles = []
+
     while True:
-        vehicle_count=0
+        vehicle_count = 0
         # Capture frame-by-frame
         ret, frame = cap.read()
 
@@ -55,8 +152,7 @@ def start_vehicle_detection():
                 class_id = np.argmax(scores)
                 confidence = scores[class_id]
 
-                if confidence > 0.5 and class_id==2:
-                    vehicle_count+=1
+                if confidence > 0.5 and class_id == 2:
                     center_x = int(detection[0] * frame.shape[1])
                     center_y = int(detection[1] * frame.shape[0])
                     width = int(detection[2] * frame.shape[1])
@@ -65,13 +161,26 @@ def start_vehicle_detection():
                     x = int(center_x - width / 2)
                     y = int(center_y - height / 2)
 
-                    boxes.append([x, y, width, height])
-                    confidences.append(float(confidence))
-                    class_ids.append(class_id)
+                    box = [x, y, width, height]
+                    overlaps = False
+                    for vehicle in detected_vehicles:
+                        # threshold=compute_best_threshold(vehicle, box)
+                        threshold=0.9
+                        if box_overlap(vehicle, box,threshold):
+                            overlaps = True
+                            break
+
+                    if not overlaps:
+                        vehicle_count += 1
+                        detected_vehicles.append(box)
+
+                        boxes.append(box)
+                        confidences.append(float(confidence))
+                        class_ids.append(class_id)
 
         # Draw bounding boxes around detected objects
         indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-        
+
         for i in indices:
             i = i
             box = boxes[i]
@@ -79,8 +188,8 @@ def start_vehicle_detection():
             cv2.rectangle(frame, (left, top),
                         (left+width, top+height), (0, 255, 0), 2)
             label = f"{class_ids[i]}: {confidences[i]:.2f}"
-            
-            cv2.putText(frame , label, (left, top-5),
+
+            cv2.putText(frame, label, (left, top-5),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             # cv2.putText(frame, label1, (left, top),
             label1 = f"Vehicle count: {vehicle_count}"
@@ -101,3 +210,5 @@ def start_vehicle_detection():
 
 # Start the vehicle detection loop when the Django app starts
 start_vehicle_detection()
+
+
